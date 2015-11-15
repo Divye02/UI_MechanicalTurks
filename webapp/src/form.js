e7cae8e4ce6e6a666479b7bfcd5ef4266d178f47
Divye Pratap Jain <dpjain@uw.edu>
2015-11-14 20:26:37 -0800

    minor xhanges

--- webapp/src/form.js
+++ webapp/src/form.js
@@ -1,60 +1,8 @@
-/**
- * @jsx React.DOM
- */
-
-var App = React.createClass({
-  getInitialState: function() {
-    return {
-      currentPage: 2,
-      pages: 0,
-      file: 'example.pdf'
-    };
-  },
-  prevPage: function(ev) {
-    ev.preventDefault();
-    this.setState({
-      currentPage: this.state.currentPage > 1 ? this.state.currentPage - 1 : 1
-    });
-  },
-  nextPage: function(ev) {
-    ev.preventDefault();
-    if(this.state.pages < this.state.pages)
-      this.setState({currentPage: this.state.pages < this.state.pages ? this.state.currentPage + 1 : this.state.pages });
-  },
-  onFileChange: function(ev) {
-    this.setState({
-      file: ev.target.files[0]
-    });
-  },
-  render: function() {
-    return (
-      <div className="container">
-        <h1>PDF.js + React = &lt;3</h1>
-        <div>
-          <label>
-            Change file<br />
-            <input type="file" onChange={this.onFileChange} />
-          </label>
-        </div>
-        <PDF page={this.state.currentPage} file={this.state.file} onDocumentComplete={this._onDocumentComplete} />
-        <div>
-          <button onClick={this.prevPage}>Previous page</button>
-          <button onClick={this.nextPage}>Next page</button>
-        </div>
-      </div>
-    );
-  },
-  _onDocumentComplete: function(pages){
-    this.setState({pages: pages});
-  }
-});
-
-React.render(<App />, document.body);
 
 
 
 
-/*
+
 
 
 
@@ -262,7 +210,6 @@ React.render(<SubmitB /> , document.getElementById('main'));
 
 
 
-*/
 
 
 
