ccf896565bc1166cc1e45aba5f6f0f67831c3112
Ubuntu <ubuntu@ip-172-31-17-45.us-west-2.compute.internal>
2015-12-04 10:18:39 +0000

    tryin to add db

--- webapp/routes/data.js
+++ webapp/routes/data.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('dbui', 'postgres', null, {dialect: 'postgres', port: 5432});
+var Bbinfo = require('../models/bbInfo');
+/* POST bbinfo listing. */
 router.post('/', function(req, res, next) {
       console.log(req.body);
       var data = req.body;
       res.send(data);
+	sequelize.sync({force: true}).then(function(){return Bbinfo.create({istext: data["isItText"], text: data["Text"], pageno: data["FontSize"], keywords: data["keywords"], bbcoordinates: data["position"] }); }).then(function(data){
+	res.send(data);
+});
 });
 
 
